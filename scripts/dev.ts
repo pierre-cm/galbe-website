6b64092349dcffee59a84674cdf650c42f428791
Pierre Caillaud Menéndez <pcaillaudm@gmail.com>
2024-01-05 00:04:49 +0100

    feat: update build script

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,92 +1,22 @@
 import yaml from "js-yaml"
-import { readdirSync, renameSync, existsSync, rmSync, mkdirSync } from "fs"
-import { resolve, basename, extname, dirname } from "path"
+import { resolve, extname } from "path"
 import { watch } from "fs"
 import config from "../config"
+import { buildSitemap } from "./fetch"
 
 const isGit = extname(config?.repository) === ".git"
-if (isGit) {
-  if (existsSync(".docs")) {
-    let proc = Bun.spawn({
-      cwd: ".docs",
-      cmd: ["git", "pull"],
-      stdout: "inherit",
-    })
-    await proc.exited
-  } else {
-    let proc = Bun.spawn({
-      cmd: [
-        "git",
-        "clone",
-        "-n",
-        "--depth=1",
-        "--filter=tree:0",
-        config.repository,
-        ".docs",
-      ],
-      stdout: "inherit",
-    })
-    await proc.exited
-    proc = Bun.spawn({
-      cwd: ".docs",
-      cmd: ["git", "sparse-checkout", "set", "--no-cone", "docs"],
-      stdout: "inherit",
-    })
-    await proc.exited
-    proc = Bun.spawn({
-      cwd: ".docs",
-      cmd: ["git", "checkout"],
-      stdout: "inherit",
-    })
-    await proc.exited
-    readdirSync(".docs/docs").forEach((file) => {
-      renameSync(resolve(".docs/docs", file), resolve(".docs", file))
-    })
-    rmSync(".docs/docs", { recursive: true, force: true })
-  }
-}
-const repoPath = isGit ? ".docs" : `${config?.repository}/docs`
-
 const sitemapStr = await Bun.file("sitemap.yaml").text()
 const sitemap = yaml.load(sitemapStr) as Record<string, any>
 
-const buildSitemap = async (
-  sitemap: Record<
-    string,
-    {
-      label: string
-      title?: string
-      description?: string
-      editUrl?: string
-      md?: string
-      items?: any
-    }
-  >,
-  path: string,
-  filter: string[] = []
-) => {
-  for (const [name, val] of Object.entries(sitemap || {})) {
-    if ("md" in val) {
-      const src = resolve(repoPath, val?.md || "")
-      const dest = resolve(path, basename(src))
-      if (filter?.length === 0 || filter.includes(src)) {
-        let f = await Bun.file(src).text()
-        f = `---\ntitle: ${val?.title}\ndescription: ${val?.description}\neditUrl: ${config.git}/blob/main/docs/${val?.md}\n---\n${f}`
-        console.log("Writing", dest)
-        mkdirSync(dirname(dest), { recursive: true })
-        Bun.write(dest, f)
-      }
-    } else if ("items" in val) {
-      buildSitemap(val.items, `${path}/${name}`, filter)
-    }
-  }
+const srcDir = resolve(import.meta.dir, "..", config?.repository, "docs")
+if (!isGit) {
+  watch(srcDir, (event, filename) => {
+    console.log(`Detected ${event} in ${resolve(srcDir, filename)}`)
+    buildSitemap(sitemap, resolve("src/content/docs"), [
+      resolve(srcDir, filename),
+    ])
+  })
 }
-await buildSitemap(sitemap, resolve("src/content/docs"))
-
-watch(resolve(import.meta.dir, "../.."), (event, filename) => {
-  console.log(`Detected ${event} in ${resolve(filename)}`)
-  buildSitemap(sitemap, resolve("src/content/docs"), [resolve(filename)])
-})
 
 Bun.spawn({
   cmd: ["bunx", "astro", "dev"],

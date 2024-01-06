200eb748e04dc1e015dd81964e1480711f7bf7de
Pierre Caillaud Menéndez <pcaillaudm@gmail.com>
2024-01-06 18:38:11 +0100

    fix: fix config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export default {
-  repository: Bun.env.KADRE_REPO,
+  repository: process.env.KADRE_REPO,
   git: {
-    token: Bun.env.GH_TOKEN,
-    repo: Bun.env.GH_REPO,
+    token: process.env.GH_TOKEN,
+    repo: process.env.GH_REPO,
   },
-  site: Bun.env.KADRE_SITE,
-  base: Bun.env.KADRE_BASE,
+  site: process.env.KADRE_SITE,
+  base: process.env.KADRE_BASE,
 }

f32cc1323faa69cf5df261a604d567309f63112d
Pierre Caillaud Menéndez <pcaillaudm@gmail.com>
2024-03-09 01:30:25 +0100

    fix: missing doc import

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -5,6 +5,8 @@ import _4 from '$lib/contents/plugins.md?raw';
 import _5 from '$lib/contents/router.md?raw';
 import _6 from '$lib/contents/routes.md?raw';
 import _7 from '$lib/contents/schemas.md?raw';
+import _8 from '$lib/contents/context.md?raw';
+import _9 from '$lib/contents/error-handler.md?raw';
 
 export default {
 	'getting-started': _1,
@@ -13,5 +15,7 @@ export default {
 	plugins: _4,
 	router: _5,
 	routes: _6,
-	schemas: _7
+	schemas: _7,
+	context: _8,
+	'error-handler': _9
 } as Record<string, string>;

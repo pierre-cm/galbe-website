85b26fa84874c4e19931765352aa5c30a9e280b3
Pierre Caillaud Menéndez <pcaillaudm@gmail.com>
2024-03-09 14:25:59 +0100

    reset GH_TOKEN

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
 	repository: Bun.env.GALBE_REPO,
 	git: {
-		token: Bun.env.GITHUB_TOKEN,
+		token: Bun.env.GH_TOKEN,
 		repo: Bun.env.GH_REPO
 	},
 	site: `https://${Bun.env.GALBE_DOMAIN}`,
